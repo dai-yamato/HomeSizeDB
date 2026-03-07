c673c4683a5767a1ebaa2906e803af38ef4da10a
d.yamato <yamaguchi.dai@goody-tech.com>
2026-03-07 10:29:30 +0900

    日本語化

---
+++ lang/ja/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'パスワードをリセットしました。',
+    'sent' => 'パスワードリセットメールを送信しました。',
+    'throttled' => '時間を置いて再度お試しください。',
+    'token' => 'このパスワードリセットトークンは無効です。',
+    'user' => 'メールアドレスに一致するユーザーが見つかりません。',
+
+];
